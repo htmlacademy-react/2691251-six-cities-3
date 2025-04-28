fc6a15bf585ab74473d8263ab1df264ef1423fb7
cooperfirewalk <cooperfirewalk@gmail.com>
2025-04-29 00:39:43 +0300

    добавляет offer-page

--- src/components/offer/offer.tsx
+++ src/components/offer/offer.tsx
@@ -6,8 +6,7 @@ function Offer(): JSX.Element {
       </div>
       <div className="cities__image-wrapper place-card__image-wrapper">
         <a href="#">
-          <img className="place-card__image" src="img/apartment-01.jpg" width="260" height="200"
-            alt="Place image" />
+          <img className="place-card__image" src="img/apartment-01.jpg" width="260" height="200" alt="Place image" />
         </a>
       </div>
       <div className="place-card__info">
@@ -34,7 +33,7 @@ function Offer(): JSX.Element {
         </h2>
         <p className="place-card__type">Apartment</p>
       </div>
-    </article>)
+    </article>);
 }
 
-export default Offer
+export default Offer;
--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,11 +1,11 @@
-import Offer from "../../components/offer/offer";
+import Offer from '../../components/offer/offer';
 
 type MainPageProps = {
   offersCount: number;
 }
 
-function MainPage({offersCount}: MainPageProps): JSX.Element {
-  const offersArray = new Array(offersCount)
+function MainPage({ offersCount }: MainPageProps): JSX.Element {
+  const offersArray = new Array(offersCount);
   return (
     <div className="page page--gray page--main">
       <header className="header">
@@ -13,7 +13,7 @@ function MainPage({offersCount}: MainPageProps): JSX.Element {
           <div className="header__wrapper">
             <div className="header__left">
               <a className="header__logo-link header__logo-link--active">
-                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41"/>
+                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
               </a>
             </div>
             <nav className="header__nav">
@@ -96,7 +96,7 @@ function MainPage({offersCount}: MainPageProps): JSX.Element {
                 </ul>
               </form>
               <div className="cities__places-list places__list tabs__content">
-              {offersArray.fill('').map(() => Offer())}
+                {offersArray.fill('').map(() => Offer())}
               </div>
             </section>
             <div className="cities__right-section">
