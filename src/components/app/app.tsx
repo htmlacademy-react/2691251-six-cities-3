3e66a7e27e6ad41486fc8d2da38bd05109c454b6
cooperfirewalk <cooperfirewalk@gmail.com>
2025-05-06 01:07:16 +0300

    добавляет роутер (без страницы с параметром)

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,12 +1,48 @@
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import { AppRoute, AuthorizationStatus } from '../../const';
 import MainPage from '../../pages/main-page/main-page';
+import LoginPage from '../../pages/login-page/login-page';
+import OfferPage from '../../pages/offer-page/offer-page';
+import FavoritesPage from '../../pages/favorites-page/favorites-page';
+import PrivateRoute from '../private-route/private-route';
+import NotFoundPage from '../../pages/not-found-page/not-found-page';
 
 type AppProps = {
   offersCount: number;
 }
 
-function App({offersCount}: AppProps): JSX.Element {
+function App({ offersCount }: AppProps): JSX.Element {
   return (
-    <MainPage offersCount={offersCount}/>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element={<MainPage offersCount={offersCount} />}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<LoginPage />}
+        />
+        <Route
+          path={AppRoute.Favorites}
+          element={
+            <PrivateRoute
+              authorizationStatus={AuthorizationStatus.NoAuth}
+            >
+              <FavoritesPage />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path={AppRoute.Offer}
+          element={<OfferPage />}
+        />
+        <Route
+          path="*"
+          element={<NotFoundPage />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
