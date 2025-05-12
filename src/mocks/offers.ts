697026d92e94e4acd9af44a780238d86c664823c
cooperfirewalk <cooperfirewalk@gmail.com>
2025-05-12 22:01:52 +0300

    чинит линтер, добавляет моки офферов + типы

---
+++ src/mocks/offers.ts
@@ -0,0 +1,95 @@
+import { Offer } from '../types/offer';
+
+export const offers: Offer[] = [
+  {
+    'id': '314769d4-5f2e-4cdb-998e-d7e38c173d34',
+    'title': 'Wood and stone place',
+    'type': 'room',
+    'price': 227,
+    'previewImage': 'https://15.design.htmlacademy.pro/static/hotel/3.jpg',
+    'city': {
+      'name': 'Paris',
+      'location': {
+        'latitude': 48.85661,
+        'longitude': 2.351499,
+        'zoom': 13
+      }
+    },
+    'location': {
+      'latitude': 48.868610000000004,
+      'longitude': 2.342499,
+      'zoom': 16
+    },
+    'isFavorite': false,
+    'isPremium': false,
+    'rating': 4.1
+  },
+  {
+    'id': '3a40cafb-015e-4323-92f4-c94f1fb7f679',
+    'title': 'Tile House',
+    'type': 'hotel',
+    'price': 202,
+    'previewImage': 'https://15.design.htmlacademy.pro/static/hotel/9.jpg',
+    'city': {
+      'name': 'Paris',
+      'location': {
+        'latitude': 48.85661,
+        'longitude': 2.351499,
+        'zoom': 13
+      }
+    },
+    'location': {
+      'latitude': 48.858610000000006,
+      'longitude': 2.330499,
+      'zoom': 16
+    },
+    'isFavorite': false,
+    'isPremium': true,
+    'rating': 2
+  },
+  {
+    'id': '56bfb061-f365-4e4a-bcf7-8128243e6bf3',
+    'title': 'Amazing and Extremely Central Flat',
+    'type': 'hotel',
+    'price': 217,
+    'previewImage': 'https://15.design.htmlacademy.pro/static/hotel/13.jpg',
+    'city': {
+      'name': 'Paris',
+      'location': {
+        'latitude': 48.85661,
+        'longitude': 2.351499,
+        'zoom': 13
+      }
+    },
+    'location': {
+      'latitude': 48.834610000000005,
+      'longitude': 2.335499,
+      'zoom': 16
+    },
+    'isFavorite': false,
+    'isPremium': false,
+    'rating': 4
+  },
+  {
+    'id': 'ccd87b72-e6b5-4d04-9e9e-11420a141dc1',
+    'title': 'Canal View Prinsengracht',
+    'type': 'apartment',
+    'price': 122,
+    'previewImage': 'https://15.design.htmlacademy.pro/static/hotel/6.jpg',
+    'city': {
+      'name': 'Paris',
+      'location': {
+        'latitude': 48.85661,
+        'longitude': 2.351499,
+        'zoom': 13
+      }
+    },
+    'location': {
+      'latitude': 48.85761,
+      'longitude': 2.358499,
+      'zoom': 16
+    },
+    'isFavorite': false,
+    'isPremium': true,
+    'rating': 2.1
+  }];
