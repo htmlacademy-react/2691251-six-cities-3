d623bbebd301a4e079dbd4d4f8e502a20487be3e
cooperfirewalk <cooperfirewalk@gmail.com>
2025-05-29 15:00:40 +0300

    расширяет моки, добавляет отрисовку офферов в зависимости от выбранного города

--- src/components/cities-list/cities-list.tsx
+++ src/components/cities-list/cities-list.tsx
@@ -1,3 +1,6 @@
+import { MouseEvent } from 'react';
+import { changeCity } from '../../store/action';
+import { useAppDispatch } from '../../hooks';
 
 type CitiesListProps = {
   citiesList: string[];
@@ -5,13 +8,23 @@ type CitiesListProps = {
 }
 
 function CitiesList({ citiesList, selectedCity }: CitiesListProps): JSX.Element {
+
+  const dispatch = useAppDispatch();
+
+  const onCityClick = (evt: MouseEvent<HTMLSpanElement>) => {
+    evt.preventDefault();
+    if (evt.currentTarget.textContent) {
+      dispatch(changeCity({ city: evt.currentTarget.textContent }));
+    }
+  };
+
   return (
     <ul className="locations__list tabs__list">
       {citiesList.map((city, id) => {
         const keyValue = `${id}-${city}`;
         return (
           <li key={keyValue} className="locations__item">
-            <a className={`locations__item-link tabs__item ${city === selectedCity ? 'tabs__item--active' : ''}`}>
+            <a onClick={onCityClick} className={`locations__item-link tabs__item ${city === selectedCity ? 'tabs__item--active' : ''}`}>
               <span>{city}</span>
             </a>
           </li>
