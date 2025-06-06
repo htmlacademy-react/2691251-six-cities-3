4c4b1ffdf0490c44311d06cdc10ac6eee89f1308
cooperfirewalk <cooperfirewalk@gmail.com>
2025-06-06 15:41:00 +0300

    добавляет асинхронное действие проверки авторизации

--- src/store/api-actions.ts
+++ src/store/api-actions.ts
@@ -2,8 +2,8 @@ import {AxiosInstance} from 'axios';
 import {createAsyncThunk} from '@reduxjs/toolkit';
 import {AppDispatch, State} from '../types/state.js';
 import {Offer} from '../types/offer.js';
-import {loadOffers, setOffersDataLoadingStatus} from './action';
-import {APIRoute} from '../const';
+import {loadOffers, requireAuthorization, setOffersDataLoadingStatus} from './action';
+import {APIRoute, AuthorizationStatus} from '../const';
 
 export const fetchOffersAction = createAsyncThunk<void, undefined, {
   dispatch: AppDispatch;
@@ -16,5 +16,21 @@ export const fetchOffersAction = createAsyncThunk<void, undefined, {
     const {data} = await api.get<Offer[]>(APIRoute.Offers);
     dispatch(setOffersDataLoadingStatus(false));
     dispatch(loadOffers(data));
+  },
+);
+
+export const checkAuthAction = createAsyncThunk<void, undefined, {
+  dispatch: AppDispatch;
+  state: State;
+  extra: AxiosInstance;
+}>(
+  'user/checkAuth',
+  async (_arg, {dispatch, extra: api}) => {
+    try {
+      await api.get(APIRoute.Login);
+      dispatch(requireAuthorization(AuthorizationStatus.Auth));
+    } catch {
+      dispatch(requireAuthorization(AuthorizationStatus.NoAuth));
+    }
   },
 );
