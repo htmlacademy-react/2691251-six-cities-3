89923ac0c57798866b186271a4ebd4a8b222d120
cooperfirewalk <cooperfirewalk@gmail.com>
2025-06-07 13:47:47 +0300

    добавляет компонент header

--- src/pages/favorites-page/favorites-page.tsx
+++ src/pages/favorites-page/favorites-page.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import { AppRoute } from '../../const';
+import Header from '../../components/header/header';
 import { Offer } from '../../types/offer';
 import { useAppSelector } from '../../hooks';
 import FavoritesEmpty from '../../components/favorites-empty/favorites-empty';
@@ -21,32 +21,7 @@ function FavoritesPage(): JSX.Element {
 
     <div className="page">
       <header className="header">
-        <div className="container">
-          <div className="header__wrapper">
-            <div className="header__left">
-              <Link className="header__logo-link" to={AppRoute.Main}>
-                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
-              </Link>
-            </div>
-            <nav className="header__nav">
-              <ul className="header__nav-list">
-                <li className="header__nav-item user">
-                  <Link className="header__nav-link header__nav-link--profile" to="#">
-                    <div className="header__avatar-wrapper user__avatar-wrapper">
-                    </div>
-                    <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                    <span className="header__favorite-count">{filteredOffers.length}</span>
-                  </Link>
-                </li>
-                <li className="header__nav-item">
-                  <Link className="header__nav-link" to={AppRoute.Login}>
-                    <span className="header__signout">Sign out</span>
-                  </Link>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
+        <Header />
       </header>
       {filteredOffers.length === 0 ? <FavoritesEmpty /> :
         <main className="page__main page__main--favorites">
--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,7 +1,6 @@
 import OffersList from '../../components/offers-list/offers-list';
 import SortList from '../../components/sort-list/sort-list';
-import { AppRoute } from '../../const';
-import { Link } from 'react-router-dom';
+import Header from '../../components/header/header';
 import { useState } from 'react';
 import { CITIES } from '../../mocks/cities';
 import Map from '../../components/map/map';
@@ -39,32 +38,7 @@ function MainPage(): JSX.Element {
   return (
     <div className="page page--gray page--main">
       <header className="header">
-        <div className="container">
-          <div className="header__wrapper">
-            <div className="header__left">
-              <Link to='#' className="header__logo-link header__logo-link--active">
-                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
-              </Link>
-            </div>
-            <nav className="header__nav">
-              <ul className="header__nav-list">
-                <li className="header__nav-item user">
-                  <Link className="header__nav-link header__nav-link--profile" to={AppRoute.Favorites}>
-                    <div className="header__avatar-wrapper user__avatar-wrapper">
-                    </div>
-                    <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                    <span className="header__favorite-count">3</span>
-                  </Link>
-                </li>
-                <li className="header__nav-item">
-                  <Link className="header__nav-link" to={AppRoute.Login}>
-                    <span className="header__signout">Sign out</span>
-                  </Link>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
+        <Header />
       </header>
 
       <main className="page__main page__main--index">
